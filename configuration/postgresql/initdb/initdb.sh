e780f15408e6e47939f9476066c562c6d3da399c
beginneronpy <gruianichita@gmail.com>
2018-04-30 19:16:54 +0300

    Removed lorawan addon.

--- configuration/postgresql/initdb/initdb.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-set -e
-
-psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" <<-EOSQL
-    create role lorawan with login password 'lorawan';
-    create database test with owner lorawan;
-    create extension pg_trgm;
-EOSQL
