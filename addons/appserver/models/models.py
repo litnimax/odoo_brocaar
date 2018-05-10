9e1c9a093f7434b9755a62577633f06f0e9d7041
Maxim Litnitskiy <litnimaxster@gmail.com>
2018-05-10 18:26:26 +0300

    id test

--- addons/appserver/models/models.py
+++ addons/appserver/models/models.py
@@ -265,6 +265,8 @@ class DeviceProfile(models.Model):
     updated_at = fields.Datetime(required=True)
     name = fields.Char(size=100, required=True)
 
+
+
 class Device(models.Model):
     _name = 'appserver.device'
     _table = 'device'
@@ -278,11 +280,27 @@ class Device(models.Model):
     # TODO device_profile_id must be defined as indicated below
     # device_profile_id = fields.Many2one(required=True, comodel_name='appserver.device_profile', ondelete='cascade')
     device_profile_id = fields.Char()
+    device_profile_id_ = fields.Many2one(string='Device Profile',
+                                         comodel='appserver.device_profile', 
+                                         compute='_get_device_profile_id',
+                                         ondelete='cascade')
     name = fields.Char(size=100, required=True)
     description = fields.Text(required=True)
     last_seen_at = fields.Datetime(required=True)
     device_status_battery = fields.Integer(required=True)
     device_status_margin = fields.Integer(required=True)
+
+
+    @api.mutli
+    def _get_device_profile_id(self):
+        for self in self:
+            device_profile = self.env['appserver.device_profile'].search(
+                            [('device_profile_id','=', self.device_profile_id)])
+            if device_profile:
+                self.device_profile_id_ = device_profile.id
+            else:
+                self.device_profile_id_ = False
+
 
 class DeviceKeys(models.Model):
     _name = 'appserver.device_keys'
