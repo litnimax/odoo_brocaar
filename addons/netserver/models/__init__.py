67e3eb0913ac29f11c901a5f1df85cc890fe142a
beginneronpy <gruianichita@gmail.com>
2018-05-15 16:14:25 +0300

    Fix models import in netserver init file.

--- addons/netserver/models/__init__.py
+++ addons/netserver/models/__init__.py
@@ -1,3 +1,4 @@
 # -*- coding: utf-8 -*-
 
-from . import models
+from . import channel_configuration, device, device_activation, device_profile, device_queue, extra_channel, \
+    frame_log, gateway, gateway_profile, gateway_profile_extra_channel, gateway_stats, routing_profile, service_profile
